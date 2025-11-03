b7293232821e3edfe4a4f480137d504417173e62
dopaemon <polarisdp@gmail.com>
2025-11-03 01:14:35 +0000

    diting: Add Device Infomation
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- lineage_diting.mk
+++ lineage_diting.mk
@@ -19,6 +19,16 @@ TARGET_SUPPORTS_QUICK_TAP := true
 BYPASS_CHARGE_SUPPORTED := false
 TARGET_DEFAULT_PIXEL_LAUNCHER := true
 
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.mist.display=1220 x 2712, 120 hz
+    ro.mist.battery=5000mah
+    ro.mist.soc=Snapdragon® 8+ Gen 1
+    ro.mist.camera=200MP + 8MP + 2MP
+    ro.mist.front=20MP
+    ro.mist.platform=SM8450
+    ro.mist.screen=6.67' AMOLED
+    ro.mist.device.name=Xiaomi 12T Pro
+
 PRODUCT_NAME := lineage_diting
 PRODUCT_DEVICE := diting
 PRODUCT_MANUFACTURER := Xiaomi
